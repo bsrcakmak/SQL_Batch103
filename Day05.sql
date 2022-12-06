5fabdc6bacf7556b15c4c75a1eca1609f1132327
Busra Cakmak <bsrckmk516@gmail.com>
2022-12-07 00:08:47 +0300

    SQL

--- Day05.sql
+++ Day05.sql
@@ -39,7 +39,10 @@ GROUP BY sirket;
     --- HAVING CLAUSE --- 
 --HAVING, AGGREGATE FUNCTION’lar ile birlikte kullanilan FILTRELEME komutudur
 -- bir gruplama olacak sonrada egerli bir istek olacak buda having icine yazilacak
-
+/*
+	Having komutu yalnizca GROUP BY komutu ile kullanilir
+	Eger gruplamadan once bir sart varsa 'where', gruplamadan sonra sart varsa 'having' kullaniyoruz.
+*/
 
 --Her sirketin MIN maaslarini eger 4000’den buyukse goster
 select * from personel;
@@ -55,16 +58,160 @@ group by isim
 having sum(maas) > 10000;
 
 --Eger bir sehirde calisan personel sayisi 1’den coksa sehir ismini 
---ve personel sayisini veren sorguyaziniz
+--ve personel sayisini veren sorgu yaziniz
 select sehir,count(isim) AS sum_personel_sayisi from personel
 group by sehir
 having count(isim) > 1;
 
+--Eger bir sehirde alinan MAX maas 5000’den dusukse sehir ismini 
+--ve MAX maasi veren sorguyaziniz
+select sehir, max(maas) AS max_maas from personel 
+GROUP BY sehir
+HAVING max(maas) < 5000;
+
+
+  -- UNION OPERATOR --
+/*
+Iki farkli sorgulamanin sonucunu birlestiren islemdir. 
+Secilen Field Sayisi ve DATA TYPE’i ayni olmalidir.
+*/
+
+--Maasi 4000’den cok olan isci isimlerini 
+--ve 5000 liradan fazla maas alinan sehirlerigosteren sorguyuyaziniz
+
+select sehir AS sehir_veya_personel_ismi, maas from personel
+where maas>5000
+UNION
+select isim AS sehir_veya_personel_ismi, maas from personel
+where maas>4000;
+
+--Mehmet Ozturk ismindeki kisilerin aldigi maaslari ve Istanbul’daki personelinmaaslarini
+--bir tabloda gosteren sorguyaziniz
+
+select isim AS isim_veya_sehir_ismi, maas from personel
+where isim='Mehmet Ozturk'
+UNION
+select sehir AS isim_veya_sehir_ismi, maas from personel
+where sehir='Istanbul';
+
+--NOT:2.sorgunun sonuna ORDER BY komutunu kullanirsaniz tum tabloyu istediginiz siralamaya gore siralar
+ORDER BY maas;
+
+
+--Sehirlerden odenen ucret 3000’den fazla olanlari 
+--ve personelden ucreti 5000’den azolanlari bir tabloda maas miktarina gore sirali olarak gosteren sorguyuyaziniz
+select sehir AS personel_veya_sehir_ismi ,maas from personel
+where maas>3000
+UNION
+select isim AS personel_veya_sehir_ismi ,maas from personel
+where maas<5000
+ORDER BY maas;
+
+
+/*
+Personel isminde bir tablo olusturun.Icinde id,isim,sehir,maas ve sirket field’lari olsun. 
+Id’yi 2.yontemle PK yapin
+*/
+
+DROP TABLE IF EXISTS personel
+
+CREATE TABLE personel
+(
+id int,
+isim varchar(50),  
+sehir varchar(50), 
+maas int,  
+sirket varchar(20),
+CONSTRAINT personel_pk PRIMARY KEY (id)
+);
+
+INSERT INTO personel VALUES(123456789, 'Ali Yilmaz', 'Istanbul', 5500, 'Honda');  
+INSERT INTO personel VALUES(234567890, 'Veli Sahin', 'Istanbul', 4500, 'Toyota');  
+INSERT INTO personel VALUES(345678901, 'Mehmet Ozturk', 'Ankara', 3500, 'Honda');  
+INSERT INTO personel VALUES(456789012, 'Mehmet Ozturk', 'Izmir', 6000, 'Ford');  
+INSERT INTO personel VALUES(567890123, 'Mehmet Ozturk', 'Ankara', 7000, 'Tofas');  
+INSERT INTO personel VALUES(456715012, 'Veli Sahin', 'Ankara', 4500, 'Ford');  
+INSERT INTO personel VALUES(123456710, 'Hatice Sahin', 'Bursa', 4500, 'Honda');
+
+CREATE TABLE personel_bilgi  (
+id int,
+tel char(10) UNIQUE ,  
+cocuk_sayisi int,
+CONSTRAINT personel_bilgi_fk FOREIGN KEY (id) REFERENCES personel(id)
+);
+
+INSERT INTO personel_bilgi VALUES(123456789, '5302345678', 5);  
+INSERT INTO personel_bilgi VALUES(234567890, '5422345678', 4);
+INSERT INTO personel_bilgi VALUES(345678901, '5354561245', 3);
+INSERT INTO personel_bilgi VALUES(456789012, '5411452659', 3);
+INSERT INTO personel_bilgi VALUES(567890123, '5551253698', 2);
+INSERT INTO personel_bilgi VALUES(456789012, '5524578574', 2);
+INSERT INTO personel_bilgi VALUES(123456710, '5537488585', 1);
+
+select * from personel
+select * from personel_bilgi
+
+--id’si 12345678 olan personelin Personel tablosundan sehir ve maasini,
+--personel_bilgi tablosundan da tel ve cocuk sayisiniyazdirin
+
+select sehir as sehir_veya_tel, maas as maas_veya_cocuk_sayisi from personel
+where id=123456789
+UNION
+select tel,cocuk_sayisi from personel_bilgi
+where id=123456789
+
+/*
+NOT : Union islemi yaparken;
+1)Her 2 QUERY’den elde edeceginiz tablolarin sutun sayilari esit olmali
+2)Alt alta gelecek sutunlarin data type’lari ayni olmali
+*/
+
+
+  --- UNION ALL OPERATOR ---
+/*
+--> UNION islemi 2 veya daha cok SELECT isleminin sonuc KUMELERINI birlestirmek icin kullanilir,
+Ayni kayit birden fazla olursa, sadece bir tanesinialir.
+--> UNION ALL ise tekrarli elemanlari, tekrar sayisinca yazar.
+NOT : UNION ALL ile birlestirmelerde de
+1)Her 2 QUERY’den elde edeceginiz tablolarin sutun sayilari esit olmali
+2)Alt alta gelecek sutunlarin data type’lari ayni olmali
+*/
 
 
 
+--Personel tablosundada maasi 5000’den az olan tum isimleri ve maaslaribulunuz
+select isim,maas from personel where maas<5000
+UNION  -- tekrar eden verileri teke dusurur 
+select isim,maas from personel where maas<5000
+
+select isim,maas from personel where maas<5000
+UNION ALL -- iki sorguyu da alt alta yazar ayni sorgu olsa bile 
+select isim,maas from personel where maas<5000
 
 
+/*
+1) Tabloda personel maasi 4000’den cok olan tum sehirleri ve maaslariyazdirin
+2) Tabloda personel maasi 5000’den az olan tum isimleri ve maaslariyazdirin
+--> Yukaridaki sorgulari UNION ALL ile birlestirin
+*/
+select sehir,maas from personel where maas>4000 
+UNION ALL
+select isim,maas from personel where maas<5000
+
+
+    -- INTERSECT(Kesisim) OPERATOR -- 
+/*
+SQL INTERSECT operatörü, 2 veya daha fazla SELECT ifadesinin sonuçlarını döndürmek için kullanılır. 
+Ancak, yalnızca tüm sorgular veya veri kümeleri tarafından seçilen satırları döndürür. 
+Bir sorguda bir kayıt varsa ve diğerinde yoksa, INTERSECT sonuçlarından çıkarılacaktır.
+*/
+
+--Personel tablosundan Istanbul veya Ankara’da calisanlarin id’lerini yazdir
+--Personel_bilgi tablosundan 2 veya 3 cocugu olanlarin id leriniyazdirin
+--Iki sorguyu INTERSECT ilebirlestirin
+select id from personel where sehir in('Istanbul','Ankara')
+INTERSECT -- iki kumenin kesisimini alir
+select id from personel_bilgi where cocuk_sayisi in(2,3);
 
 
 
--- Team02.sql
+++ Team02.sql
@@ -1,27 +1,31 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Her markanin id’sini, ismini ve toplam kaç şehirde bulunduğunu listeleyen bir SORGU yaziniz.
+​
+select * from calisanlar2;
+select * from markalar;
+​
+select marka_id,marka_isim, (select count(sehir) from calisanlar2 where isyeri=marka_isim)
+from markalar
+​
+-- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin toplam maaşini listeleyiniz
+​
+SELECT marka_isim,calisan_sayisi, (SELECT sum(maas) from calisanlar2 where isyeri=marka_isim) as maaslartoplami
+FROM markalar
+​
+-- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin maksimum ve minumum maaşini listeleyen bir Sorgu yaziniz.
+​
+SELECT marka_isim,calisan_sayisi,
+(SELECT min(maas) from calisanlar2 where isyeri=marka_isim)as minimummaas,
+(SELECT max(maas) from calisanlar2 where isyeri=marka_isim)as maximummaas
+from markalar;
+​
+​
 CREATE TABLE mart
 (
 urun_id int,
 musteri_isim varchar(50),
 urun_isim varchar(50)
 );
-
+​
 INSERT INTO mart VALUES (10, 'Mark', 'Honda');
 INSERT INTO mart VALUES (20, 'John', 'Toyota');
 INSERT INTO mart VALUES (30, 'Amy', 'Ford');
@@ -29,69 +33,67 @@ INSERT INTO mart VALUES (20, 'Mark', 'Toyota');
 INSERT INTO mart VALUES (10, 'Adam', 'Honda');
 INSERT INTO mart VALUES (40, 'John', 'Hyundai');
 INSERT INTO mart VALUES (20, 'Eddie', 'Toyota');
+​
 CREATE TABLE nisan
 (
 urun_id int ,
 musteri_isim varchar(50),
 urun_isim varchar(50)
 );
-
+​
 INSERT INTO nisan VALUES (10, 'Hasan', 'Honda');
 INSERT INTO nisan VALUES (10, 'Kemal', 'Honda');
 INSERT INTO nisan VALUES (20, 'Ayse', 'Toyota');
 INSERT INTO nisan VALUES (50, 'Yasar', 'Volvo');
 INSERT INTO nisan VALUES (20, 'Mine', 'Toyota');
-
+​
 /*
 MART VE NİSAN aylarında aynı URUN_ID ile satılan ürünlerin
 URUN_ID’lerini listeleyen ve aynı zamanda bu ürünleri MART ayında alan
 MUSTERI_ISIM 'lerini listeleyen bir sorgu yazınız.
 */
-
-select musteri_isim,urun_id from mart    -- IN ile cozum
-where urun_id IN(select urun_id from nisan)
-
-
-select musteri_isim,urun_isim from mart  -- EXISTS ile cozum
-where exists (select urun_isim from nisan where mart.urun_isim=nisan.urun_isim);
-
-
+select * from mart
+select * from nisan
+​
+select musteri_isim,urun_isim from mart where urun_isim IN (select urun_isim from nisan)
+​
+SELECT musteri_isim,urun_isim from mart where exists (select urun_isim from nisan where mart.urun_isim=nisan.urun_isim)
+​
 --Her iki ayda birden satılan ürünlerin URUN_ISIM'lerini ve bu ürünleri
 --NİSAN ayında satın alan MUSTERI_ISIM'lerini listeleyen bir sorgu yazınız.
-
-select urun_isim,musteri_isim from nisan 
-where exists (select urun_isim from mart where nisan.urun_isim=mart.urun_isim);
-
-
+​
+Select urun_isim,musteri_isim from nisan where exists (SELECT urun_isim from mart where nisan.urun_isim=mart.urun_isim)
+​
 --Her iki ayda ortak satilmayan ürünlerin URUN_ISIM'lerini ve bu ürünleri
 --NİSAN ayında satın alan MUSTERI_ISIM'lerini listeleyen bir sorgu yazınız.
-
-select urun_isim,musteri_isim from nisan 
-where not exists (select urun_isim from mart where nisan.urun_isim=mart.urun_isim);
-
-
-CREATE TABLE tedarikciler --parent
+​
+select urun_isim,musteri_isim from nisan where not exists (select urun_isim from mart where nisan.urun_isim=mart.urun_isim)
+​
+--UPDATE SET
+​
+CREATE TABLE tedarikciler -- parent
 (
 vergi_no int PRIMARY KEY,
 firma_ismi VARCHAR(50),
 irtibat_ismi VARCHAR(50)
 );
-drop table if exists tedarikciler
-CREATE TABLE urunler --child
+​
+INSERT INTO tedarikciler VALUES (101, 'IBM', 'Kim Yon');
+INSERT INTO tedarikciler VALUES (102, 'Huawei', 'Çin Li');
+INSERT INTO tedarikciler VALUES (103, 'Erikson', 'Maki Tammen');
+INSERT INTO tedarikciler VALUES (104, 'Apple', 'Adam Eve');
+​
+CREATE TABLE urunler -- child
 (
 ted_vergino int,
 urun_id int,
 urun_isim VARCHAR(50),
 musteri_isim VARCHAR(50),
-CONSTRAINT fk_urunler FOREIGN KEY(ted_vergino) REFERENCES tedarikciler(vergi_no)
+CONSTRAINT fk_urunler FOREIGN KEY(ted_vergino)
+REFERENCES tedarikciler(vergi_no)
 on delete cascade
-	
 );
-INSERT INTO tedarikciler VALUES (101, 'IBM', 'Kim Yon');
-INSERT INTO tedarikciler VALUES (102, 'Huawei', 'Çin Li');
-INSERT INTO tedarikciler VALUES (103, 'Erikson', 'Maki Tammen');
-INSERT INTO tedarikciler VALUES (104, 'Apple', 'Adam Eve');
-
+​
 INSERT INTO urunler VALUES(101, 1001,'Laptop', 'Ayşe Can');
 INSERT INTO urunler VALUES(102, 1002,'Phone', 'Fatma Aka');
 INSERT INTO urunler VALUES(102, 1003,'TV', 'Ramazan Öz');
@@ -99,19 +101,38 @@ INSERT INTO urunler VALUES(102, 1004,'Laptop', 'Veli Han');
 INSERT INTO urunler VALUES(103, 1005,'Phone', 'Canan Ak');
 INSERT INTO urunler VALUES(104, 1006,'TV', 'Ali Bak');
 INSERT INTO urunler VALUES(104, 1007,'Phone', 'Aslan Yılmaz');
-
---vergi_no’su 102 olan tedarikcinin firma ismini 'Vestel' olarak güncelleyeniz.
-select * from tedarikciler;
-select * from urunler;
-
-update tedarikciler
-set firma_ismi='Vestel' where vergi_no='102';
-
-
---vergi_no’su 101 olan tedarikçinin firma ismini 'casper' ve irtibat_ismi’ni 'Ali Veli' olarak güncelleyiniz.
-
-update tedarikciler
-set firma_ismi='Casper',irtibat_ismi='Ali Veli' where vergi_no='101';`
-
-
-
+​
+-- vergi_no’su 102 olan tedarikcinin firma ismini 'Vestel' olarak güncelleyeniz.
+select * from tedarikciler
+select * from urunler
+​
+UPDATE tedarikciler
+SET firma_ismi='Vestel' where vergi_no=102
+​
+-- vergi_no’su 101 olan tedarikçinin firma ismini 'casper' ve irtibat_ismi’ni 'Ali Veli' olarak güncelleyiniz.
+​
+UPDATE tedarikciler
+SET firma_ismi='Casper', irtibat_ismi='Ali Veli' where vergi_no=101
+​
+-- urunler tablosundaki 'Phone' değerlerini 'Telefon' olarak güncelleyiniz.
+Update urunler
+SET urun_isim='Telefon' where urun_isim='Phone'
+​
+-- urunler tablosundaki urun_id değeri 1004'ten büyük olanların urun_id’sini 1 arttırın.
+​
+UPDATE urunler
+SET urun_id=urun_id+1 where urun_id>1004
+​
+-- urunler tablosundaki tüm ürünlerin urun_id değerini ted_vergino sutun değerleri ile toplayarak güncelleyiniz.
+UPDATE urunler
+SET urun_id=urun_id+ted_vergino 
+​
+-- urunler tablosundan Ali Bak’in aldigi urunun ismini, tedarikci tablosunda irtibat_ismi
+--'Adam Eve' olan firmanın ismi (firma_ismi) ile degistiriniz.
+​
+UPDATE urunler
+SET urun_isim = (SELECT firma_ismi from tedarikciler where irtibat_ismi='Adam Eve') WHERE musteri_isim='Ali Bak';
+​
+--* Urunler tablosunda laptop satin alan musterilerin ismini, firma_ismi Apple’in irtibat_isim'i ile degistirin.
+UPDATE urunler
+SET musteri_isim = (SELECT irtibat_ismi from tedarikciler where firma_ismi='Apple') WHERE urun_isim='Laptop'
